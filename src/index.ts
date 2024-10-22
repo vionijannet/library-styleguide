14926d7795f2b5c111245679022a98b7b48e895a
Vioni <vionijannet@gmail.com>
2024-10-22 14:40:30 +0700

    Add input to index.ts
    - Update version

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
-import { Plugin, App } from 'vue';
 import './style.css';
+
+import { Plugin, App } from 'vue';
 import Button from './components/Button.vue';
+import Input from './components/Input.vue';
 
 const plugin: Plugin = {
     install(app: App) {
         app.component("ButtonGista", Button);
+        app.component("InputGista", Input);
     }
 };
 
